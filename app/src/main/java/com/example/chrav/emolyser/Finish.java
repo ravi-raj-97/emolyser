33e4b54027974b2abe7410a00c48f73b7a50a597
ravi-raj-97 <raviraj97@gmail.com>
2017-05-25 17:07:18 +0530

    UI Final Commit

--- app/src/main/java/com/example/chrav/emolyser/Finish.java
+++ app/src/main/java/com/example/chrav/emolyser/Finish.java
@@ -1,7 +1,10 @@
 package com.example.chrav.emolyser;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class Finish extends AppCompatActivity {
 
@@ -9,5 +12,18 @@ public class Finish extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_finish);
+
+        Button bt1 = (Button) findViewById(R.id.button33);
+        bt1.setOnClickListener(new View.OnClickListener()
+        {
+            @Override
+            public void onClick(View v)
+            {
+                Intent homeIntent = new Intent(Intent.ACTION_MAIN);
+                homeIntent.addCategory( Intent.CATEGORY_HOME );
+                homeIntent.setFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+                startActivity(homeIntent);
+            }
+        });
     }
 }
